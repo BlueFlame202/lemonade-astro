cca11173c5ab009ffcb3b786042ddf80ca9811e9
Aathreya Kadambi <aathreyakadambi@gmail.com>
2024-08-05 10:21:43 -0700

    fixing integrations oops

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,7 @@ import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
 
-// // https://astro.build/config
-// export default defineConfig({
-//   site: 'https://blueflame202.github.io/',
-//   base: 'lemonade-astro',
-//   integrations: [tailwind(), react(), mdx()]
-// });
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react(), mdx()]
+});
